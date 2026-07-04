a20bc5fb4b97ffd420c3350be10c23f059908213
lengocthanh9238-star <lengocthanh9238@gmail.com>
2026-07-04 21:02:38 +0800

    Create password_generator.py

---
+++ password_generator.py
@@ -0,0 +1,8 @@
+import random
+import string
+
+length = 12
+chars = string.ascii_letters + string.digits + "!@#$%^&*"
+
+password = "".join(random.choice(chars) for _ in range(length))
+print(password)
